dcd7b131ba32d917368aebdcedaa83b156449acf
student <student@macbook-air.nwmissouri.edu>
2018-10-04 18:24:07 -0500

    demo

--- Women In Computing/FirstViewController.swift
+++ Women In Computing/FirstViewController.swift
@@ -17,7 +17,7 @@ class FirstViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated.sruti
     }
 
 
