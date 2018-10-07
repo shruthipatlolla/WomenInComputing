d60caddd4edeb90073de35caa7d1b8b4c740de04
Nista Shrestha <sthanista@icloud.com>
2018-10-07 16:13:31 -0500

    Worked on Nav bar icons

--- Women In Computing/FamousWomenTableViewController.swift
+++ Women In Computing/FamousWomenTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class FamousWomenTableViewController: UITableViewController {
 let names = ["Maxres", "Edith Clarke", "Grace Hopper"]
     let images = [#imageLiteral(resourceName: "maxresdefault"),#imageLiteral(resourceName: "68065028_134808027444"),#imageLiteral(resourceName: "Grace")]
-    let descp = ["Edith Clarke was the first female electrical engineer at the University of Texas at Austin. She specialized in electrical power system analysis.", "Jean Jennings Bartik was one of the original programmers for the ENIAC computer. She studied mathematics in school then began manually calculating ballistics trajectories", "Grace Brewster Murray Hopper was an American computer scientist. she was a pioneer of computer programming"]
+    let descp = ["Maxres was one of the original programmers for the ENIAC computer. She studied mathematics in school then began manually calculating ballistics trajectories", "Edith Clarke was the first female electrical engineer at the University of Texas at Austin. She specialized in electrical power system analysis.", "Grace Brewster Murray Hopper was an American computer scientist. she was a pioneer of computer programming"]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
