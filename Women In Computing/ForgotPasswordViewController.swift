734bf8e0a0fb2e89f27bcced6fccbcbc21e0c340
Appasani Satya Phani Shyam <S531512@nwmissouri.edu>
2018-11-28 16:42:39 -0600

    Appasani Satya Phani Shyam - added password recovery functionality

--- Women In Computing/ForgotPasswordViewController.swift
+++ Women In Computing/ForgotPasswordViewController.swift
@@ -18,12 +18,29 @@ class ForgotPasswordViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func onRecoverPassword(_ sender: Any) {
-        //let email = emailTF.text!
-        self.dismiss(animated: true, completion: nil)
-
-        // need to work
+    func display(title:String, msg:String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
+    @IBAction func onRecoverPassword(_ sender: Any) {
+        let email = emailTF.text! as String
+        Types.tryblock({ () -> Void in
+            
+            let result = Backendless.sharedInstance().userService.restorePassword(email)
+            //print("Check your email address! result = \(result)")
+            self.display(title: "Success", msg: "Check your email address!")
+        },
+                       
+                       catchblock: { (exception) -> Void in
+                        self.display(title: "Alert", msg: "Invalid Email")
+                        //print("Server reported an error: \(exception as! Fault)")
+        })
+        
+    }
+    
+   
     
     @IBAction func onCancel(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
--- Women In Computing/LoginViewController.swift
+++ Women In Computing/LoginViewController.swift
@@ -48,8 +48,6 @@ class LoginViewController: UIViewController {
             
         })
         
-        //        }
-        
     }
     
     
